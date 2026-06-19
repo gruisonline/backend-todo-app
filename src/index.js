88a07193a4fe979d9e81607dd18c1efa122006fb
Dasharath Chaudhary <dasharathchaudhary61399@gmail.com>
2026-06-19 10:21:24 +0530

    connect database, confing dotenv, add express and middleware confing and created ApiError, ApiResponse and asyncHandler utils functions

---
+++ src/index.js
@@ -0,0 +1,20 @@
+import dotenv from "dotenv";
+import { connectDB } from "./db/index.js";
+import { app } from "./app.js";
+
+dotenv.config({
+  path: "./.env"
+})
+
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Server is running at port: ${process.env.PORT || 8000}`)
+    })
+    app.on("error", (error) => {
+      console.log(`Error accured: ${error}`)
+    })
+  })
+  .catch((error) => {
+    console.log(`Mongo DB connection failed! || : ${error}`)
+  })
